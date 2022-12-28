9aaf568d7705b8529d6f388669e29dff760e60fa
gudebin <gudebin@outlook.com>
2022-12-28 22:22:07 +0800

    Site updated: 2022-12-28 22:22:07

--- .umirc.ts
+++ .umirc.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from "umi";
 
 export default defineConfig({
-  npmClient: 'pnpm',
+  npmClient: "pnpm",
+  tailwindcss: {},
+  plugins: ["@umijs/plugins/dist/tailwindcss"],
 });
